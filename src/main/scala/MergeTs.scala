e4b82adabfdbbb8f08e58655933e8b3b5d764314
Arnout Engelen <arnouten@bzzt.net>
2018-01-09 09:56:49 +0100

    Remove dead code

--- src/main/scala/MergeTs.scala
+++ src/main/scala/MergeTs.scala
@@ -1,16 +1,11 @@
 import java.io.PrintWriter
-import java.time.{LocalDate, ZoneOffset}
 
-import spray.json.{JsNumber, JsObject, JsString}
+import spray.json.{JsObject, JsString}
 
 import scala.io.Source
 import scala.util.Try
 
 object MergeTs extends App {
-
-//  println(classOf[App].getResourceAsStream("gw-1001.log").read())
-//  println(Thread.currentThread().getContextClassLoader().getResourceAsStream("/gw-1001.log").read())
-//  Source.fromResource(s"gw-1001.log").getLines.toList
 
   val hosts = Seq("gw-1001", "gw-2001", "gw-3001")
   val sources = hosts.flatMap(name => Source.fromResource(s"$name.log").getLines.toList.map(event => (name, event)))
@@ -31,22 +26,7 @@ object MergeTs extends App {
       }
     }.mkString("\n")
 
-//  val logs = sources
-//    .groupBy(_._1)
-//    .toList
-//    .sortBy(_._1)
-//    .zipWithIndex
-//    .flatMap {
-//    case ((ts, events), idx) =>
-//      events.map {
-//        case (_, host, event) =>
-//        s"$host|${vc(idx+1)}|$event"
-//      }
-//  }.mkString("\n")
-//
   new PrintWriter("out.logs") {
-//    write("(?<host>[^|]+)\\|(?<clock>[^|]+)\\|(?<event>.*)\\n\n")
-//    write("====\n")
     write(logs); close }
 
   def vc(ts: Int): String =
@@ -54,11 +34,6 @@ object MergeTs extends App {
 
   def parseTimestamp(logline: String): String = {
     val timestamp = logline.split("32m").tail.head.split(".\\[0").head
-//    val Array(head, millis) = timestamp.split("\\.")
-//    val Array(hours, minutes, seconds) = head.split(":")
-//    val epochSecond = LocalDate.now().atStartOfDay().toInstant(ZoneOffset.UTC).getEpochSecond
-//
-//    millis.toLong + 1000 * (epochSecond + seconds.toLong + 60 * (minutes.toLong + 60 * hours.toLong))
     "2018-01-07T" + timestamp + "Z"
   }
 
