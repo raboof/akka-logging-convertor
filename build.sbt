1e1d357b85ffa560eed70aaaa76b18f724510c66
Arnout Engelen <arnouten@bzzt.net>
2018-01-09 10:40:40 +0100

    Invent vector clocks based on timestamps

--- build.sbt
+++
@@ -1,1 +0,0 @@
-libraryDependencies += "io.spray" %%  "spray-json" % "1.3.3"
--- src/main/scala/MergeTs.scala
+++ src/main/scala/MergeTs.scala
@@ -1,65 +1,41 @@
 import java.io.PrintWriter
-import java.time.{LocalDate, ZoneOffset}
 
-import spray.json.{JsNumber, JsObject, JsString}
-
+import scala.annotation.tailrec
 import scala.io.Source
 import scala.util.Try
 
 object MergeTs extends App {
 
-//  println(classOf[App].getResourceAsStream("gw-1001.log").read())
-//  println(Thread.currentThread().getContextClassLoader().getResourceAsStream("/gw-1001.log").read())
-//  Source.fromResource(s"gw-1001.log").getLines.toList
-
   val hosts = Seq("gw-1001", "gw-2001", "gw-3001")
-  val sources = hosts.flatMap(name => Source.fromResource(s"$name.log").getLines.toList.map(event => (name, event)))
+  val sources = hosts.flatMap(host => Source.fromResource(s"$host.log").getLines.toList.map(event => (host, event)))
 
   val logs = sources.flatMap {
     case (name, event) => Try(name, event, parseTimestamp(event)).toOption.toList
-  }.map {
-      case (host, event, timestamp) => {
-        val level = event.split(" .\\[32m")(0)
-        val Array(logger, message) = event.split("\\[33m")(1).split(".\\[0;39m  ")
-        JsObject(
-          "@timestamp" -> JsString(timestamp),
-          "host" -> JsString(host),
-          "event" -> JsString(message),
-          "level" -> JsString(level),
-          "logger" -> JsString(logger),
-        ).compactPrint
-      }
-    }.mkString("\n")
+  }.sortBy(_._3).toList
 
-//  val logs = sources
-//    .groupBy(_._1)
-//    .toList
-//    .sortBy(_._1)
-//    .zipWithIndex
-//    .flatMap {
-//    case ((ts, events), idx) =>
-//      events.map {
-//        case (_, host, event) =>
-//        s"$host|${vc(idx+1)}|$event"
-//      }
-//  }.mkString("\n")
-//
   new PrintWriter("out.logs") {
-//    write("(?<host>[^|]+)\\|(?<clock>[^|]+)\\|(?<event>.*)\\n\n")
-//    write("====\n")
-    write(logs); close }
+    write("(?<host>[^|]+)\\|(?<clock>[^|]+)\\|(?<event>.*)\\n\n")
+    write("====\n")
+    @tailrec
+    def writeAll(logs: List[(String, String, Long)], currentTimestamps: Map[String, Long]): Unit = logs match {
+      case Nil => // Done.
+      case (host, event, _) :: xs =>
+        val nextTimestamps = currentTimestamps.updated(host, currentTimestamps.getOrElse(host, 0L) + 1L)
+        write(s"$host|${vc(nextTimestamps)}|$event\n")
+        writeAll(xs, nextTimestamps)
+    }
+    writeAll(logs, Map.empty)
+    close
+  }
 
-  def vc(ts: Int): String =
-    hosts.map(host => s""""$host": $ts""").mkString("{", ",", "}")
+  def vc(ts: Map[String, Long]): String =
+    hosts.flatMap(host => ts.get(host).map(t => s""""$host": $t""")).mkString("{", ",", "}")
 
-  def parseTimestamp(logline: String): String = {
+  def parseTimestamp(logline: String): Long = {
     val timestamp = logline.split("32m").tail.head.split(".\\[0").head
-//    val Array(head, millis) = timestamp.split("\\.")
-//    val Array(hours, minutes, seconds) = head.split(":")
-//    val epochSecond = LocalDate.now().atStartOfDay().toInstant(ZoneOffset.UTC).getEpochSecond
-//
-//    millis.toLong + 1000 * (epochSecond + seconds.toLong + 60 * (minutes.toLong + 60 * hours.toLong))
-    "2018-01-07T" + timestamp + "Z"
+    val Array(head, millis) = timestamp.split("\\.")
+    val Array(hours, minutes, seconds) = head.split(":")
+    millis.toLong + 1000 * (seconds.toLong + 60 * (minutes.toLong + 60 * hours.toLong))
   }
 
 
