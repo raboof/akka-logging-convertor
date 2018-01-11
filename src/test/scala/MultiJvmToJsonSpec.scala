334b83933234c3157d7a4b8161c69c72ceb4802b
Arnout Engelen <arnouten@bzzt.net>
2018-01-11 12:01:11 +0100

    Adding 'role' and 'address' fields

--- src/test/scala/MultiJvmToJsonSpec.scala
+++ src/test/scala/MultiJvmToJsonSpec.scala
@@ -5,9 +5,13 @@ import scala.util.Success
 
 class MultiJvmToJsonSpec extends WordSpec with Matchers {
   "The multi-jvm jenkins log output parser" should {
+    
+    val roles = Map("JVM-2-FooSpec" -> "first")
+    val addresses = Map("JVM-2-FooSpec" -> "akka.tcp://FooSpec@127.0.0.1:4312")
+    
     "parse a simple line" in {
-      MultiJvmToJson.parseLine("[JVM-2] [INFO] [01/09/2018 04:07:03.676] [ScalaTest-main] [akka.remote.Remoting] Starting remoting") should
-        be(Success(Line(2, "INFO", "2018-09-01T04:07:03.676Z", "ScalaTest", "main", "akka.remote.Remoting", "Starting remoting")))
+      MultiJvmToJson.parseLine("[JVM-2-FooSpec] [INFO] [01/09/2018 04:07:03.676] [ScalaTest-main] [akka.remote.Remoting] Starting remoting", roles, addresses) should
+        be(Success(Line(2, "FooSpec", "first", "akka.tcp://FooSpec@127.0.0.1:4312", "INFO", "2018-01-09T04:07:03.676Z", "ScalaTest-main", "akka.remote.Remoting", "Starting remoting")))
     }
   }
 }
